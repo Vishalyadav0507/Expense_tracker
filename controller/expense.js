6c56c8b5ec8f9791f403c12b5dd742f0e4986894
vishalyadav0507 <vishal364364yadav>
2023-02-25 15:20:23 +0530

    transaction

--- controller/expense.js
+++ controller/expense.js
@@ -1,19 +1,24 @@
+
 const expense = require('../model/expense')
 const User = require('../model/signup')
+const sequelize = require('../util/datbase')
+
 const postItem = async (req, res, next) => {
     try {
+        const t= await sequelize.transaction()
         const { amount, description, Item, } = req.body
         const userId = req.user.id
-        console.log(">>>", req.user)
-        const Expense = await expense.create({ amount, description, Item, userId })
+        const Expense = await expense.create({ amount, description, Item, userId },{transaction:t})
         const totalExpense = Number(req.user.total_amount) + Number(amount)
-        console.log('totaalamount', totalExpense)
         User.update({
             total_amount: totalExpense
-        }, {where: { Id: req.user.id }})
+        }, {where: { Id: req.user.id }},
+        {transaction:t})
+        await t.commit()
         res.status(201).json({ expense: Expense })
     } catch (err) {
-        res.status(404).json({ err: "something went wrong" })
+        await t.rollback()
+        res.status(501).json({ err: "something went wrong" })
     }
 }
 
@@ -29,12 +34,23 @@ const getItem = async (req, res) => {
 }
 const deleteItem = async (req, res, next) => {
     try {
+        const t=await sequelize.transaction()
         const ItemId = req.params.id
+        const gettingExpenseDetails= await expense.findAll({where:{id:ItemId}})
+        const UserDetails=await User.findAll({where:{id:gettingExpenseDetails[0].userId}})
         const response = await expense.destroy({ where: { id: ItemId, userId: req.user.id } })
         if (response == true) {
             res.status(201).json({ success: true })
+            const deletedAmount=gettingExpenseDetails[0].amount
+            const UserTotalAmount=UserDetails[0].total_amount
+            const remmainingAmount=Number(UserTotalAmount)-Number(deletedAmount)
+            User.update({
+                total_amount:remmainingAmount
+            },{where:{id:gettingExpenseDetails[0].userId}},{transaction:t})
+            await t.commit()
         }
     } catch (err) {
+        await t.rollback()
         res.status(501).json({ err: "something went wrong" })
     }
 }
